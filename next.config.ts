78bd3d0d9553d20a1024c1f90e902796c7afe5ed
sadhu2005 <sadhuj2005@gmail.com>
2025-08-09 17:35:33 +0530

    edited our yml only for github pages

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // This creates a static export of your site in an "out" folder.
   output: 'export',
-
-  // This disables the default server-based image optimization.
   images: {
     unoptimized: true,
   },
+  // ADD THIS LINE
+  basePath: 'sadhu_portfolio', // Example: '/sadhu_portfolio'
 };
 
 export default nextConfig;
